cf8eb0bb2d7b757701f5b20b7ace771747d9812c
Diwashach30 <diwashacharya30@gmail.com>
2025-01-07 20:08:19 +0545

    percentage

---
+++ subject.py
@@ -0,0 +1,15 @@
+ ##Marks of student A##
+math = 50
+
+science = 36
+english = 33
+nepali = 31
+social = 48
+total_marks = 250
+
+obtain_marks = math+science+english+nepali+social
+
+percentage = (obtain_marks / total_marks )* 100
+
+print (f"percentage of student A is {percentage:.2f}")
+ 
