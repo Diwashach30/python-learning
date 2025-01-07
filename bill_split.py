1ea29f4dc44bd4aacb0b16cddbb4b92542f0fcbf
Diwashach30 <diwashacharya30@gmail.com>
2025-01-07 19:28:59 +0545

    SI

--- bill_split.py
+++ bill_split.py
@@ -4,3 +4,24 @@ total_bill = 1300
 bill_per_person = total_bill / total_people
 
 print (f"Each people have to pay Rs {bill_per_person}")
+
+
+## create program that calculate simple interest 
+## formula = principal * rate * time / 100
+
+total_loan = 100000
+total_interest = 10
+
+interest_paid_to_bank = (total_interest /100) * (total_loan)
+
+print (f"Hence total amount need to pay for bank is Rs {interest_paid_to_bank}.")
+
+## another
+
+p = 5000
+t = 50
+r = 8
+
+SI = (p*t*r) / 100
+
+print (f" SI is Rs {SI}")
