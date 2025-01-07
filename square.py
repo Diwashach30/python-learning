f56241d802e5119ba202d1e4d0094b8a7d32dcba
Diwashach30 <diwashacharya30@gmail.com>
2025-01-07 20:43:12 +0545

    day 3

---
+++ square.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+square1 = num1 * num1
+square2 = num2 * num2
+
+print (f"square of num1 is {square1}")
+print (f"square of num2 is {square2}")
