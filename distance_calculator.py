2340e72fad6565907599f98311e5b49a16024f18
Diwashach30 <diwashacharya30@gmail.com>
2025-01-07 19:09:53 +0545

    day 2

---
+++ distance_calculator.py
@@ -0,0 +1,9 @@
+home_to_college_in_km = 40 #km
+bike_speed = 45 #km/h
+
+
+time_to_college = home_to_college_in_km / bike_speed
+ 
+in_min = time_to_college * 60
+ 
+print ( f"It will take {in_min} minutes to get to college. ")
