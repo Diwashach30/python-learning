25b9ce60a0d188d8c706cfc82a4b472cb77a0587
Diwashach30 <diwashacharya30@gmail.com>
2025-01-09 20:56:56 +0545

    condition

---
+++ voter.py
@@ -0,0 +1,21 @@
+age = 19
+
+if age >= 18:
+    
+    print ("You are voter")
+else :
+    print(" You are not voter")
+    
+    
+    #create
+     
+     
+price = float( input(" Enter  Price : "))
+
+if price >= 500 :
+    
+    print ("It's Expensive")
+else :
+    print(" It's cheap")
+    
+    
