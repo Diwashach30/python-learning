eb385c262d4549bdced403db319023a794a68ee7
Diwashach30 <diwashacharya30@gmail.com>
2025-01-10 20:56:17 +0545

    day_6_completed

---
+++ check_numberrr.py
@@ -0,0 +1,9 @@
+number = int(input("Enter  Number : "))
+
+if number > 0 :
+    print ("Number is Positive")
+elif number < 0 :
+    print ("Number is Negative")
+else  :
+    print ("Number is zero")    
+    
---
+++ day_of_week.py
@@ -0,0 +1,18 @@
+day_num = int(input("Enter Day number : "))
+
+if day_num == 1:
+    print ("Day is Sunday")
+elif day_num == 2 :
+    print ("Day is Monday")
+elif day_num == 3:
+    print ("Day is Tuesday")
+elif day_num == 4 :
+    print ("Day is Wednesday")
+elif day_num == 5 :
+    print ("Day is Thrusday")
+elif day_num == 6 :
+    print ("Day is Friday")
+elif day_num == 7 :
+    print ("Day is Saturday")
+else :
+    print ("Invalid Day")
